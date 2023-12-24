14d3ccf225705f70517c23f633328c56f012548b
Pete Halbeisen <pete.halbeisen@gmail.com>
2023-12-24 15:01:33 -0500

    Skip n/a values.

--- gps.py
+++ gps.py
@@ -1,5 +1,7 @@
 import os
+import time
 from gps3 import agps3
+
 gps_socket = agps3.GPSDSocket()
 data_stream = agps3.DataStream()
 
@@ -16,6 +18,12 @@ gps_socket.watch()
 for new_data in gps_socket:
     if new_data:
         data_stream.unpack(new_data)
+		
+	if data_stream.time is 'n/a':
+	  continue
+
         print(str(data_stream.time) + ', ' + str(data_stream.alt) + ', ' + str(data_stream.lat) + ', ' + str(data_stream.lon))
         f.write(str(data_stream.time) + ', ' + str(data_stream.alt) + ', ' + str(data_stream.lat) + ', ' + str(data_stream.lon))
         f.write("\n")
+        time.sleep(2)
+        
